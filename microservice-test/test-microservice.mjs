a17116ebb8d1bdf77cc049c852ec257c7f414663
Reif L Stanfield <spiffypirate64@gmail.com>
2024-11-18 21:05:10 +0000

    README updates

--- microservice-test/test-microservice.mjs
+++ microservice-test/test-microservice.mjs
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 const testMicroservice = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/random-city");
+    const response = await fetch("https://cs-361-microservice.vercel.app/api/random-city");
     const data = await response.json();
     console.log("Response from microservice:", data);
   } catch (error) {
